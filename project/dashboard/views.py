aca71047cccbccc681cf8be3fface222d5f99b75
Geetanjali <geetareddy2005@gmail.com>
2024-10-13 02:26:40 +0530

    modified views.py and urls.py of dashboard

--- project/dashboard/views.py
+++ project/dashboard/views.py
@@ -1,19 +1,6 @@
 from django.shortcuts import render,redirect
 
-def proxy(request):
-    if request.user.is_applicant:
-        return redirect('applicant-dashboard')
-    elif request.user.is_recruiter:
-        return redirect('recruiter-dashboard')
-    else:
-        return redirect('login')
-    
-
-def applicant_dashboard(request):
-    return render('dashboard/applicant_dashboard.html')
-
-def recruiter_dashboard(request):
-    return render('dashboard/recruiter_dashboard.html')
-
+def dashboard(request):
+    return render(request,'dashboard/dashboard.html')
 
 
