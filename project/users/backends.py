4959b446ceb3b021551d21ef75ff4630b0972080
Geetanjali <geetareddy2005@gmail.com>
2024-10-13 00:59:41 +0530

    fixed bugs in banckends.py

--- project/users/backends.py
+++ project/users/backends.py
@@ -1,16 +1,19 @@
-import django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 
-UserModel=get_user_model()
+UserModel = get_user_model()
+
 class EmailBackend(ModelBackend):
-    def authenticate(self,request, username=None,password=None, **kwargs):
+    def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user=UserModel.objects.get(Q(username_iexact=username)| Q(email_iexact=username))
+            user = UserModel.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
         except UserModel.DoesNotExist:
-            UserModel().set_password(password)
-            return
+            return None
         except UserModel.MultipleObjectsReturned:
-            user= UserModel.objects.filter(Q(username_iexact=username)| Q(email_iexact=username)).order_by('id').first()
+            user = UserModel.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).order_by('id').first()
+
         if user.check_password(password) and self.user_can_authenticate(user):
             return user
+        
+        return None
