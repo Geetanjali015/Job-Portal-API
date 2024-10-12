b79586dd53e28a47ccbb3e7ce82a94cc77a6c86a
Geetanjali <geetareddy2005@gmail.com>
2024-10-13 02:03:52 +0530

    updated views.py of users app login logout

--- project/company/models.py
+++ project/company/models.py
@@ -4,10 +4,10 @@ from users.models import User
 
 class Company(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    name=models.CharField(max_length=100)
-    est_date=models.PositiveIntegerField()
-    city=models.CharField(max_length=100)
-    state=models.CharField(max_length=100)
+    name=models.CharField(max_length=100,null=True,blank=True)
+    est_date=models.PositiveIntegerField(null=True,blank=True)
+    city=models.CharField(max_length=100,null=True,blank=True)
+    state=models.CharField(max_length=100,null=True,blank=True)
 
     def __str__(self):
         return self.name
--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'company',
     'resume',
     'users',
     'widget_tweaks'
