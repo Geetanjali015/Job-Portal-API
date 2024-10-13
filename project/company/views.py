7116fbb40aafef0a3e862fd825e7f5d86d301ae6
Geetanjali <geetareddy2005@gmail.com>
2024-10-13 23:11:25 +0530

    modifying ui and updating backend

--- project/company/views.py
+++ project/company/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import Company
+from .form import UpdateCompanyForm
+from users.models import User
 
-# Create your views here.
+
+
+#update company
+def update_company(request):
+    company=Company.objects.get(user=request.user)
+    if request.method=="POST":
+        form=UpdateCompanyForm(request.POST, instance=company)
+        if form.is_valid():
+            var=form.save(commit=False)
+            user=User.objects.get(id=request.user.id)
+            user.has_company=True
+            var.save()
+            user.save()
+            messages.info(request,'your company info has been updated')
+            return redirect('dashboard')
+
+        else:
+            messages.warning(request,'Somaething went wrong')
+    else:
+        form=UpdateCompanyForm(instance=company)
+        context={'form':form}
+        return render(request,'company/update_company.html',context)
+    
+#view company details
+def company_details(request,pk):
+    company=Company.objects.get(pk=pk)
+    context={'company':company}
+    return render(request,'company/company_details.html',context)
