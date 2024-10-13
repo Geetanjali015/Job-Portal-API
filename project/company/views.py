546e55c952ce7dede8804db9ba566cf7d497964d
Geetanjali <geetareddy2005@gmail.com>
2024-10-13 23:55:08 +0530

    facing issues with resume

--- project/company/views.py
+++ project/company/views.py
@@ -8,25 +8,28 @@ from users.models import User
 
 #update company
 def update_company(request):
-    company=Company.objects.get(user=request.user)
-    if request.method=="POST":
-        form=UpdateCompanyForm(request.POST, instance=company)
-        if form.is_valid():
-            var=form.save(commit=False)
-            user=User.objects.get(id=request.user.id)
-            user.has_company=True
-            var.save()
-            user.save()
-            messages.info(request,'your company info has been updated')
-            return redirect('dashboard')
+    if request.user.is_recruiter:
+        company=Company.objects.get(user=request.user)
+        if request.method=="POST":
+            form=UpdateCompanyForm(request.POST, instance=company)
+            if form.is_valid():
+                var=form.save(commit=False)
+                user=User.objects.get(id=request.user.id)
+                user.has_company=True
+                var.save()
+                user.save()
+                messages.info(request,'your company info has been updated')
+                return redirect('dashboard')
 
+            else:
+                messages.warning(request,'Somaething went wrong')
         else:
-            messages.warning(request,'Somaething went wrong')
+            form=UpdateCompanyForm(instance=company)
+            context={'form':form}
+            return render(request,'company/update_company.html',context)
     else:
-        form=UpdateCompanyForm(instance=company)
-        context={'form':form}
-        return render(request,'company/update_company.html',context)
-    
+        messages.warning(request,'Permission denied')
+        return redirect('dashboard')
 #view company details
 def company_details(request,pk):
     company=Company.objects.get(pk=pk)
